1f3f970a7101b32ea17fa94225466a2b8c2db30d
joanka <joasiek136@gmail.com>
2017-01-14 01:08:49 +0100

    menu section with tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,34 @@
 $(document).ready(function() {
 
-	$('.nav-btn').click(function() {
+	$('.nav-btn').on('click', function() {
       $('.main-head').toggleClass('nav-opened'); 
     });
 
-	$('.close-icon').click(function() {
+	$('.close-icon').on('click', function() {
       $('.main-head').removeClass('nav-opened'); 
     });
 
-    $('.main-head li a').click(function() {
-	      $('.main-head').toggleClass('nav-opened');    
+    $('.main-head li a').on('click', function(e) {
+        e.preventDefault();
+	    $('.main-head').toggleClass('nav-opened');    
+    });
+
+    // menu tabs
+    var tabs = $('.tabs-menu');
+    $('.tab').not(":first").hide();
+
+    tabs.find('a').on('click', function(e){
+    	e.preventDefault();
+    	tabs.find('.current').removeClass('current');
+    	$(this).addClass('current');
+    	$(this.hash).fadeIn(400).siblings().hide();
     });
 
 
 });
+
+
+
+
+
 
