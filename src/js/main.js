340708b8840b96c7c4a1428c00035057767a0774
joanka <joasiek136@gmail.com>
2017-01-19 16:07:31 +0100

    offer section with slider

--- src/js/main.js
+++ src/js/main.js
@@ -24,10 +24,40 @@ $(document).ready(function() {
     	$(this.hash).fadeIn(400).siblings().hide();
     });
 
+    // slider oferta
+  $('.control-next, .control-prev').on('click', function() {
+    var activeItem = $('.offer-items').find('.active-item'),
+       //items = $('.offer-items'),
+       position = $('.offer-items').children().index(activeItem),
+       itemNum = $('.offer-item').length; 
+
+       if ($(this).hasClass('control-next')) {
+            if (position < itemNum - 1) {
+                $('.active-item').removeClass('active-item').next().addClass('active-item');
+            } else {
+                $('.offer-item').removeClass('active-item').first().addClass('active-item');
+                $('.dot-buttons li').removeClass('active-item').first().addClass('active-item');
+            }
+       } else {
+            if (position === 0) {
+                $('.offer-item').removeClass('active-item').last().addClass('active-item');
+                $('.dot-buttons li').removeClass('active-item').last().addClass('active-item');
+            } else {
+                $('.active-item').removeClass('active-item').prev().addClass('active-item');
+            }
+       }        
+  });
+
+  $('.dot-buttons li').on('click', function() {
+      var $this = $(this), 
+      $siblings = $this.parent().children(),
+      position = $siblings.index($this);
+      $('.offer-item').removeClass('active-item').eq(position).addClass('active-item');
+      $siblings.removeClass('active-item');
+      $this.addClass('active-item');
+    });
 
 });
-
-
 
 
 
