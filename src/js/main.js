1dac5d12ea3597916dda728f01ba26ad3de5ff8e
joanka <joasiek136@gmail.com>
2017-05-22 01:37:41 +0200

    added map, footer and sticky nav bar

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,17 @@ $(document).ready(function() {
     $('.main-head').toggleClass('nav-opened');    
   });
 
+  //Sticky nav
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 80 ) {
+      $('.main-head').addClass('sticky-nav');
+      $('.top-head .top-phone').addClass('sticky-head');
+    } else {
+      $('.main-head').removeClass('sticky-nav');
+      $('.top-head .top-phone').removeClass('sticky-head');
+    }
+  });
+
   // Menu tabs
   var tabs = $('.tabs-menu');
   $('.tab').not(":first").hide();
@@ -74,9 +85,209 @@ $(document).ready(function() {
     }
   });
 
+  // Google maps
+  var styles = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#f5f5f5"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#ffffff"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#dadada"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#948676"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e5e5e5"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#eeeeee"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c9c9c9"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  }
+];
+
+  function initMap() {
+    var silverMapType = new google.maps.StyledMapType (
+      styles, {name: 'Silver Map'});
+
+    var mapContainer = document.getElementById("map");
+    var map = new google.maps.Map(mapContainer, {
+      center: { lat: 53.204799, lng: 23.207588 }, 
+      zoom: 14,
+      scrollwheel: false,
+      disableDefaultUI: true,
+      zoomControl: true,
+      zoomControlOptions: {style: google.maps.ZoomControlStyle.DEFAULT},
+    });
+
+    map.mapTypes.set('silver_map', silverMapType);
+    map.setMapTypeId('silver_map');
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(53.204799, 23.207588),
+      map: map,
+      title: 'Deska Smaków',
+      icon: new google.maps.MarkerImage("http://maps.google.com/mapfiles/kml/pal2/icon38.png")
+    });
+
+    marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+
+    var contentString = '<div style="height: auto; width: auto; padding:10px; background:#533834; color:#fff;">'+'<p>Deska Smaków</p>'+'</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+    infowindow.open(map,marker);
+
+  }
+
+  window.onload = initMap;
 
 
-  
+
 });
 
 
